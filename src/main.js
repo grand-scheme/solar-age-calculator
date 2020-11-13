833532b795ec21143cc7abccc21feeceb7a25e7d
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-13 12:17:11 -0800

    verify connectivity between html, css, and js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import './css/bootstrap.css';
 import './css/styles.css';
+
+$(document).ready(function() {
+  $("#age-input").submit(function(e) {
+    e.preventDefault();
+    $("#output-text").text($("#input").val());
+  });
+});
