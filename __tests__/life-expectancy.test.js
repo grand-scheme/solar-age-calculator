837e10102154e4f48a43f9c12bdac0f47feafa8a
Shannon Grantski <grantski@pm.me>
2020-11-18 05:20:18 -0800

    add test for non-number, update math

--- __tests__/life-expectancy.test.js
+++ __tests__/life-expectancy.test.js
@@ -7,13 +7,12 @@ describe('LifeExpectancy', () => {
   });
   test('should exclude input that is not a number', () => {
     const badLifeInput = new LifeExpectancy("jungle gym");
-    expect(badLifeInput.LifeExpectancy()).toEqual("is not a number");
-    // expect(goodLifeInput.LifeExpectancy()).toEqual("is a number");
+    expect(badLifeInput.TickTock()).toEqual("is not a number");
   });
   test('should subtract user input from static number', () => {
     expect(100 - goodLifeInput.age).toBeCloseTo(50);
   });
   test('should incorporate tested subtraction into function', () => {
-    expect(goodLifeInput.LifeExpectancy()).toBeCloseTo(50);
+    expect(goodLifeInput.TickTock()).toBeCloseTo(27);
   });
 });
