f03e4c4127df8c7bca38ff639b090875e8682ad8
Shannon Grantski <grantski@pm.me>
2020-11-18 04:51:02 -0800

    tie javascript values to html elements

--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ import Planets from './js/planets.js';
 $(document).ready(function() {
   $("#age-input").submit(function(e) {
     e.preventDefault();
-    const age = $("#input").val();
+    const age      = $("#input").val();
     const mathTime = new Planets(age);
     const response = mathTime.PlanetaryMath();
-    $("#output-text").text(response);
+    $("#output-text").show();
+    $("#mercury-age").text(response.mercury.toFixed(2));
+    $("#venus-age").text(response.venus.toFixed(2));
+    $("#earth-age").text(response.earth.toFixed(2));
+    $("#mars-age").text(response.mars.toFixed(2));
+    $("#jupiter-age").text(response.jupiter.toFixed(2));
   });
 });
