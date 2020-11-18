79821303d5b467ef7d474e6ed15f35ff50ee9d2b
Shannon Grantski <grantski@pm.me>
2020-11-18 04:09:54 -0800

    FIX MY INABILITY TO DO MATH

--- src/js/planets.js
+++ src/js/planets.js
@@ -1,10 +1,10 @@
 export default class Planets {
   constructor(age) {
     this.age     = age;
-    this.mercury = (this.age * 0.24);
-    this.venus   = (this.age * 0.62);
-    this.mars    = (this.age * 1.88);
-    this.jupiter = (this.age * 11.86);
+    this.mercury = (this.age / 0.24);
+    this.venus   = (this.age / 0.62);
+    this.mars    = (this.age / 1.88);
+    this.jupiter = (this.age / 11.86);
   }
 
   PlanetaryMath() {
