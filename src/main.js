5edf9a2bb8560e75a08251187e92424b93f86e19
Shannon Grantski <grantski@pm.me>
2020-11-18 05:21:31 -0800

    rename variables for clarity, add death timer

--- src/main.js
+++ src/main.js
@@ -3,18 +3,35 @@ import 'bootstrap';
 import './css/bootstrap.css';
 import './css/styles.css';
 import Planets from './js/planets.js';
+import LifeExpectancy from './js/life-expectancy.js';
 
 $(document).ready(function() {
   $("#age-input").submit(function(e) {
     e.preventDefault();
     const age      = $("#input").val();
-    const mathTime = new Planets(age);
-    const response = mathTime.PlanetaryMath();
+
+    const orbitCount = new Planets(age);
+    const orbits = orbitCount.PlanetaryMath();
+    
+    const earthYearCount = new LifeExpectancy(age);
+    const earthYearsRemaining = earthYearCount.TickTock();
+    const solarDeath = new Planets(earthYearsRemaining)
+
     $("#output-text").show();
-    $("#mercury-age").text(response.mercury.toFixed(2));
-    $("#venus-age").text(response.venus.toFixed(2));
-    $("#earth-age").text(response.earth.toFixed(2));
-    $("#mars-age").text(response.mars.toFixed(2));
-    $("#jupiter-age").text(response.jupiter.toFixed(2));
+
+    $("#mercury-age").text(orbits.mercury.toFixed(2));
+    $("#time-on-mercury").text(solarDeath.mercury.toFixed(2));
+
+    $("#venus-age").text(orbits.venus.toFixed(2));
+    $("#time-on-venus").text(solarDeath.venus.toFixed(2));
+
+    $("#earth-age").text(orbits.earth.toFixed(2));
+    $("#time-on-earth").text(solarDeath.earth);
+
+    $("#mars-age").text(orbits.mars.toFixed(2));
+    $("#time-on-mars").text(solarDeath.mars.toFixed(2));
+
+    $("#jupiter-age").text(orbits.jupiter.toFixed(2));
+    $("#time-on-jupiter").text(solarDeath.jupiter.toFixed(2));
   });
 });
