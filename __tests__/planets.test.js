c3efcd1c83a56432734c24986c95c624b1890fcf
Shannon Grantski <grantski@pm.me>
2020-11-18 04:00:26 -0800

    add tests for age, begin testing PlanetaryMath()

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -5,10 +5,32 @@ describe('Planets', () => {
   beforeEach(() => {
 planetsTest = new Planets(10);
   });
-  test('should store value of a number into index [0]', () => {
-    expect(planetsTest.ageC).toEqual(10);
+  test('should store value for Venus', () => {
+    expect(planetsTest.venus).toBeCloseTo(6.2);
   });
-  test('should store value in index [2]', () => {
-    expect(planetsTest.ven).toBeCloseTo(6.2);
+  test('should store value for Mercury', () => {
+    expect(planetsTest.mercury).toBeCloseTo(2.4);
+  });
+  test('should store value for Age', () => {
+    expect(planetsTest.age).toBeCloseTo(10);
+  });
+  test('should store value for Mars', () => {
+    expect(planetsTest.mars).toBeCloseTo(18.80);
+  });
+  test('should store value for Jupiter', () => {
+    expect(planetsTest.jupiter).toBeCloseTo(118.60);
+  });
+});
+
+describe('PlanetaryMath()', () => {
+  let mathTest;
+  beforeEach(() => {
+    mathTest = new Planets(100);
+  });
+  test('should return portion of planetaryObject', () => {
+    expect(mathTest.PlanetaryMath().mercury).toBeCloseTo(24);
+  });
+  test('should return different portion of planetaryObject', () => {
+    expect(mathTest.PlanetaryMath().jupiter).toBeCloseTo(1186);
   });
 });
